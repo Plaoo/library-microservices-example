bee2afef3eba37e44be19cd64f5990ce4726e7dd
Paolo <paolo.monni345@gmail.com>
2020-06-13 16:33:50 +0200

    Added author db_manager

--- book-service/app/api/service.py
+++ book-service/app/api/service.py
@@ -6,5 +6,5 @@ AUTHOR_SERVICE_HOST_URL = "http://localhost:8002/api/v1/authors"
 
 def is_author_present(book_id: int):
     url = os.environ.get("AUTHOR_SERVICE_HOST_URL") or AUTHOR_SERVICE_HOST_URL
-    r = httpx.get(f"{url}{author_id}")
+    r = httpx.get(f"{url}{authors_id}")
     return True if r.statuscode == 200 else False
