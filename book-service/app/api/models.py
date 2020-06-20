5468900bd48649fa565f8aeb400d929dd90e82dd
Paolo <paolo.monni345@gmail.com>
2020-06-20 17:47:10 +0200

    Fix author type in book

--- book-service/app/api/models.py
+++ book-service/app/api/models.py
@@ -1,18 +1,20 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
+
 class BookIn(BaseModel):
     title: str
     year: int
     genres: List[str]
-    authors_id: Optional[List[str]] = None
+    authors_id: Optional[List[int]] = None
+
 
 class BookOut(BookIn):
     id: int
+
 
 class BookUpdate(BookIn):
     title: Optional[str] = None
     year: Optional[int] = None
     genres: Optional[List[str]] = None
-    authors_id: Optional[List[str]] = None
-
+    authors_id: Optional[List[int]] = None
