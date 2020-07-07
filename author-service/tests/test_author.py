671174182f10dce1e6b0bd1309fbce54388df49f
Paolo <paolo.monni345@gmail.com>
2020-07-07 08:47:23 +0200

    Minor fix, upgrade fastapi library

--- author-service/tests/test_author.py
+++ author-service/tests/test_author.py
@@ -1,4 +1,4 @@
-from fastapi.testclient import TestClient
+from starlette.testclient import TestClient
 from app.main import app
 from app.api.author import authors
 import logging
@@ -8,9 +8,9 @@ import requests
 client = TestClient(app)
 
 def test_get_authors():
-    response = client.get("/")
+    response = client.get("http://0.0.0.0:8080/api/v1/authors/")
     assert response.status_code == 200
 
 def test_get_author():
-    response = client.get("/1")
+    response = client.get("http://0.0.0.0:8080/api/v1/authors/1")
     assert response.status_code == 200
