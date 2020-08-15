4fcbfb053b988b6462ffe2510773bb14cae1c296
Paolo <paolo.monni345@gmail.com>
2020-08-15 19:26:36 +0200

    Added Redis config

--- author-service/app/api/author.py
+++ author-service/app/api/author.py
@@ -2,43 +2,62 @@ from typing import List
 from fastapi import APIRouter, HTTPException
 from app.api.models import AuthorOut, AuthorIn, AuthorUpdate
 from app.api import db_manager
+import json
+import logging
+from fastapi.encoders import jsonable_encoder
+log = logging.getLogger(__name__)
+from aiocache import cached, Cache
+from aiocache.serializers import JsonSerializer, PickleSerializer
 
 authors = APIRouter()
 
-@authors.get('/', response_model=List[AuthorOut])
-async def get_authors():
-    return await db_manager.get_all_authors()
 
-@authors.post('/', response_model=AuthorOut, status_code=201)
+@authors.get("/", response_model=List[AuthorOut])
+@cached(
+    ttl=10,
+    cache=Cache.REDIS,
+    endpoint="192.168.1.54",
+    serializer=JsonSerializer(),
+    port=6379,
+    namespace="main",
+    #key="key",
+)
+async def get_authors():
+    return jsonable_encoder(await db_manager.get_all_authors())
+
+
+@authors.post("/", response_model=AuthorOut, status_code=201)
 async def create_author(payload: AuthorIn):
     author_id = await db_manager.add_author(payload)
     response = {"id": author_id, **payload.dict()}
     return response
 
-@authors.get('/{id}', response_model=AuthorOut)
-async def get_author(id:int):
+
+@authors.get("/{id}", response_model=AuthorOut)
+@cached(ttl=100)
+async def get_author(id: int):
     author = await db_manager.get_author(id)
     if not author:
-        raise HTTPException(status_code=404, detail='Author not Found')
+        raise HTTPException(status_code=404, detail="Author not Found")
     return author
 
-@authors.put('/{id}', response_model=AuthorOut)
-async def update_author(id:int, payload: AuthorUpdate):
+
+@authors.put("/{id}", response_model=AuthorOut)
+async def update_author(id: int, payload: AuthorUpdate):
     author = await db_manager.get_author(id)
     if not author:
-        raise HTTPException(status_code=404, detail='Author not Found')
-    update_data = payload.dict(exclude_unset = True)
+        raise HTTPException(status_code=404, detail="Author not Found")
+    update_data = payload.dict(exclude_unset=True)
     author_id_db = AuthorIn(**author)
 
-    update_author= author_id_db.copy(update=update_data)
+    update_author = author_id_db.copy(update=update_data)
     update = await db_manager.update_author(id, update_author)
     return await get_author(id)
 
+
 @authors.delete("/{id}", response_model=None)
 async def delete_author(id: int):
-    author= await db_manager.get_author(id)
+    author = await db_manager.get_author(id)
     if not author:
         raise HTTPException(status_code=404, detail="Author not found")
     return await db_manager.delete_author(id)
-
-
--- author-service/tests/test_author.py
+++ author-service/tests/test_author.py
@@ -1,16 +1,27 @@
 from starlette.testclient import TestClient
 from app.main import app
 from app.api.author import authors
-import logging
-log = logging.getLogger('__name__')
-import requests
 
 client = TestClient(app)
 
-def test_get_authors():
-    response = client.get("http://0.0.0.0:8080/api/v1/authors/")
+
+def test_create_author():
+    dummy_data = {
+        "firstname": "Stephen",
+        "surname": "King",
+        "year_of_birth": 1947,
+        "nationality": "USA",
+    }
+
+    response = client.post("http://172.30.0.1/api/v1/authors", json=dummy_data)
     assert response.status_code == 200
 
-def test_get_author():
-    response = client.get("http://0.0.0.0:8080/api/v1/authors/1")
-    assert response.status_code == 200
+
+#def test_get_authors():
+#    response = client.get("http://localhost/api/v1/authors")
+#    assert response.status_code == 200
+
+
+# def test_get_author():
+#    response = requests.get("http://0.0.0.0:8000/api/v1/authors/1")
+#    assert response.status_code == 200
